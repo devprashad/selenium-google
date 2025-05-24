aa870d00cefd8ebfdd2d2d42b985f7d2830ec162
devprashad <kdpdev2004@gmail.com>
2025-05-24 16:41:33 +0530

    more changes done

--- selenium_webapp.py
+++ selenium_webapp.py
@@ -8,9 +8,10 @@ app = Flask(__name__)
 
 def download_selenium():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument(" -- headless")
-    chrome_options.add_argument(" -- nc-sandbox")
-    chrome_options.add_argument(" -- disable-cev-she-usage")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     driver.get("https://google.com")
     title = driver.title
@@ -23,5 +24,5 @@ def hame():
     if (request.method == 'GET'):
         return download_selenium()
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     app.run(debug=True, port=3000)
