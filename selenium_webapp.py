1bfd3e02c174623edddfe87559dda556c8d0b690
devprashad <kdpdev2004@gmail.com>
2025-05-24 17:19:16 +0530

    change in py

--- selenium_webapp.py
+++ selenium_webapp.py
@@ -2,40 +2,40 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import os
 import subprocess
 
-print("CHROME VERSION:", subprocess.getoutput("chromium --version"))
-print("CHROMEDRIVER VERSION:", subprocess.getoutput("chromedriver --version"))
-
 app = Flask(__name__)
 
+@app.route('/', methods=['GET', 'POST'])
+def hame():
+    if request.method == 'GET':
+        return jsonify(download_selenium())
+
 def download_selenium():
-    chrome_options = webdriver.ChromeOptions()
+    print("CHROME VERSION:", subprocess.getoutput("chromium --version"))
+    print("CHROMEDRIVER VERSION:", subprocess.getoutput("chromedriver --version"))
+
+    chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.binary_location = "/usr/bin/chromium"
 
-
-    # Use system-installed ChromeDriver
     service = Service("/usr/bin/chromedriver")
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
     driver.get("https://google.com")
     title = driver.title
-    language = driver.find_element(By.XPATH, "//div[@id='SIvCob']").text
+    try:
+        language = driver.find_element(By.XPATH, "//div[@id='SIvCob']").text
+    except Exception as e:
+        language = "Language info not found: " + str(e)
     driver.quit()
 
     return {'Page Title': title, 'Language': language}
 
-@app.route('/', methods=['GET', 'POST'])
-def hame():
-    if request.method == 'GET':
-        return download_selenium()
-
-# Fix main block
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
